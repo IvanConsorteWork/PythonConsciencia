5e38de2dd3b53147b8ed98f1f8a2ef3dcaa0d35c
AntilopeDisecado <IvanConsorte@yahoo.com.ar>
2023-06-20 17:02:10 -0300

    terminada, falta revisión nomas

--- guia5.py
+++ guia5.py
@@ -9,10 +9,12 @@
 
 def ordenamiento_burbuja():
 
+  ingreso = input('Ingresa una serie de 10 números separados por espacios a ordenar').split()
+
   lista = []
 
-  for i in range (0, 10):
-    numero = int(input(f'Ingresa un número entero (faltan {10 - i} ingresos \n>>> '))
+  for numero in ingreso:
+    numero = int(numero)
     lista.append(numero)
 
   for i in range (1, len(lista)):
@@ -37,11 +39,8 @@ ordenamiento_burbuja()
 
 def ordenar_alfabeticamente():
 
-  lista = []
-
-  for i in range (0, 10):
-    palabra = input(f'Ingresa una palabra (faltan {10 - i} ingresos \n>>> ')
-    lista.append(palabra)
+  lista = input('Ingrese una serie de 10 palabras separadas por espacios ha ser ordenadas alfabéticamente')
+  lista = lista.split(" ")
 
   lista.sort()
 
@@ -64,17 +63,28 @@ ordenar_alfabeticamente()
 # lista = [55, 66, 44, 77, 33, 88, 22, 99, 11, 45]
 
 def ordenamiento_insercion():
+  comprobado = False
+  
   lista = []
-  cantidad_numeros_restantes = 8
 
-  while cantidad_numeros_restantes > 0:
-    try:
-      numero = int(input(f'Ingresa un número entero (faltan {cantidad_numeros_restantes} ingresos \n>>> '))
-      lista.append(numero)
-      cantidad_numeros_restantes -= 1
-    except:
-      print('Error: Solo serán válidos los ingresos de números enteros')
-      continue
+  while comprobado == False:
+    ingreso = input('Ingresa una serie de 8 números separados por espacios a ordenar').split()
+
+    error = False
+
+    for numero in ingreso:
+      try:
+        numero = int(numero)
+      except:
+        print('Error: Uno de los ingresos no fue un número válido')
+        error = True
+    
+    if error == False:
+      comprobado = True    
+    
+  for numero in ingreso:
+    numero = int(numero)
+    lista.append(numero)
 
   for i in range (1, len(lista)):
     j = i    
@@ -104,17 +114,28 @@ ordenamiento_insercion()
 # lista = [55, 66, 44, 77, 33, 88, 22, 99, 11, 45]
 
 def ordenamiento_seleccion():
+  comprobado = False
+  
   lista = []
-  cantidad_numeros_restantes = 8
 
-  while cantidad_numeros_restantes > 0:
-    try:
-      numero = int(input(f'Ingresa un número entero (faltan {cantidad_numeros_restantes} ingresos \n>>> '))
-      lista.append(numero)
-      cantidad_numeros_restantes -= 1
-    except:
-      print('Error: Solo serán válidos los ingresos de números enteros')
-      continue
+  while comprobado == False:
+    ingreso = input('Ingresa una serie de 8 números separados por espacios a ordenar').split()
+
+    error = False
+
+    for numero in ingreso:
+      try:
+        numero = int(numero)
+      except:
+        print('Error: Uno de los ingresos no fue un número válido')
+        error = True
+    
+    if error == False:
+      comprobado = True    
+    
+  for numero in ingreso:
+    numero = int(numero)
+    lista.append(numero)
 
   for j in range (0, len(lista) - 1):
     i_min = j
@@ -136,7 +157,7 @@ ordenamiento_seleccion()
 
 # Búsqueda
 
-# Ejercicio N° 1
+# Ejercicio N° 5
 
 # Escribe una función que tome una lista de números y un valor objetivo, y realice una búsqueda lineal para determinar si el valor objetivo está presente en la lista. La función debe devolver True si el valor objetivo se encuentra en la lista y False en caso contrario.
 # %%
@@ -155,14 +176,14 @@ print(busqueda_linear(lista, 56))
 
 # - - - - - - - - - - - - - - - - - - 
 
-# Ejercicio N° 2
+# Ejercicio N° 6
 
 # Escribe una función que tome una lista de 12 números ordenados de forma ascendente y un valor objetivo, y realice una búsqueda binaria para determinar si el valor objetivo está presente en la lista.
 
 # La función debe devolver True si el valor objetivo se encuentra en la lista y False en caso contrario.
 # %%
 
-lista = [11, 22, 33, 44, 55, 66, 77, 88, 99, 101]
+lista = [11, 22, 33, 44, 55, 66, 77, 88, 99, 110, 121, 132]
 
 def busqueda_binaria(lista, numero_objetivo):
   indice_mitad = len(lista) // 2
@@ -181,5 +202,80 @@ def busqueda_binaria(lista, numero_objetivo):
 print(busqueda_binaria(lista, 55))
 print(busqueda_binaria(lista, 56))
 print(busqueda_binaria(lista, 11))
+
+# - - - - - - - - - - - - - - - - - - 
+
+# Ejercicio N° 7
+
+# Escribe un programa que solicite al usuario ingresar una lista de números separados por espacios. Luego, pide al usuario que ingrese un número específico y utiliza el método index() para encontrar el índice de la primera
+# aparición de ese número en la lista. Muestra el índice encontrado al usuario.
+# %%
+
+def busqueda_indice():
+  comprobado = False
+  
+  lista = []
+
+  while comprobado == False:
+    ingreso = input('Ingresa una serie de 10 números separados por espacios').split()
+
+    error = False
+
+    for numero in ingreso:
+      try:
+        numero = int(numero)
+      except:
+        print('Error: Uno de los ingresos no fue un número válido \n>>>')
+        error = True
+    
+    if error == False:
+      comprobado = True    
+    
+  for numero in ingreso:
+    numero = int(numero)
+    lista.append(numero)
+
+  numero_objetivo = int(input('Ingrese el número ha ser buscado'))
+
+  try:
+    indice = lista.index(numero_objetivo)
+    print(f'El número se halla en la posición {indice}')
+  except:
+    print('El número no se halla en la lista')
+    
+busqueda_indice(lista, 55)
+busqueda_indice(lista, 56)
+busqueda_indice(lista, 11)
+
+# - - - - - - - - - - - - - - - - - - 
+
+# Ejercicio N° 8
+
+# Escribe una función que tome una lista de objetos (por ejemplo, objetos de una clase particular) y un valor objetivo, y realice una búsqueda para determinar si algún objeto de la lista tiene una propiedad o atributo específico igual al valor objetivo. La función debe devolver True si se encuentra un objeto con la propiedad deseada y False en caso contrario.
+# %%
+
+def busqueda_objetos(numero_objetivo):
+
+  class Nodo:
+    def __init__(self, item):
+      self.valor = item
+    def __repr__(self):
+      return str(self.valor)
+
+  lista = list()
+
+  for i in range (1, 10):
+    nodo = Nodo(i)
+    lista.append(nodo)
+
+  for elemento in lista:
+    if elemento.valor == numero_objetivo:
+      return True
+
+  return False
+
+print(busqueda_objetos(3))
+print(busqueda_objetos(77))
+print(busqueda_objetos(7))
 
 # %%
